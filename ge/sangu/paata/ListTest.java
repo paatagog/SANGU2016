911592a3ab2f4b768f024fadc6db9aa5301ec583
Paata Gogishvili <paatagog@gmail.com>
2016-10-06 13:11:34 +0400

    first commit

---
+++ ge/sangu/paata/ListTest.java
@@ -0,0 +1,11 @@
+package ge.sangu.paata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 201 on 10/6/2016.
+ */
+public class ListTest {
+    List<String> sia = new ArrayList<String>();
+}
