06ffc5c61cc18af460a92e3520f31e518a335d59
Paata Gogishvili <paatagog@gmail.com>
2016-10-06 13:53:50 +0400

    modificirebuli failebis atvirtva

--- ge/sangu/paata/ListTest.java
+++ ge/sangu/paata/ListTest.java
@@ -11,5 +11,19 @@ public class ListTest {
     public static void main(String[] args) {
         List<String> sia = new ArrayList<String>();
         sia.add("paata");
+        sia.add("giorgi");
+        sia.add("kukusha");
+        sia.remove(1);
+        for (int i = 0; i < sia.size(); i++) {
+            System.out.println(sia.get(i));
+        }
+        for (String s: sia) {
+            System.out.println(s);
+        }
+
+        // დავალება: ზემოთ მოყვანილი ორი ცუდი მეთოდის ნაცვლად გააკეთეთ ეკრანზე სიის ბეჭდვა
+        // უფრო ელეგანტური მეთოდით. არ გამოიყენოთ System.out.println(sia); ბრძანება
+
     }
+
 }
---
+++ ge/sangu/paata/SetTest.java
@@ -0,0 +1,21 @@
+package ge.sangu.paata;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Created by 201 on 10/6/2016.
+ */
+public class SetTest {
+
+    public static void main(String[] args) {
+        Set<String> simravle = new HashSet<String>();
+        simravle.add("paata");
+        simravle.add("guga");
+        simravle.add("paata");
+        System.out.println(simravle);
+
+
+    }
+}
