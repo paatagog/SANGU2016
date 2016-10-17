5ae5b7c7400572fc2213ab774e41b0cefe38f164
kavtaradze <guga.kavtaradze@sangu.edu.ge>
2016-10-17 16:25:29 +0400

    Hello world - first homework

---
+++ ge/sangu/kavtaradze/HelloWorld.java
@@ -0,0 +1,10 @@
+package ge.sangu.kavtaradze;
+
+/**
+ * Created by Guga on 10/17/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
