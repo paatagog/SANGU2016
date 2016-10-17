95508108ef53da76581cb5bd48e41e3f4dcdbb54
Nikoloz995 <nikoloz.astamidze@gmail.com>
2016-10-17 15:17:20 +0400

    sheivala JAva 1

--- ge/sangu/Testing/Test1.java
+++ ge/sangu/Testing/Test1.java
@@ -5,6 +5,6 @@ package ge.sangu.Testing;
  */
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello World2");
     }
 }
