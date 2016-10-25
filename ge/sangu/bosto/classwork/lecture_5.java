99c8b2d2296020cda8ab7d5ee6e414de1cb4eb78
natinati585 <natiabosto@yahoo.com>
2016-10-25 15:38:10 +0400

    hkh

---
+++ ge/sangu/bosto/classwork/lecture_5.java
@@ -0,0 +1,10 @@
+package ge.sangu.bosto.classwork;
+
+/**
+ * Created by natia on 10/25/2016.
+ */
+public class lecture_5 {
+    public static void main(String[] args) {
+        System.out.println("bla");
+    }
+}
